828c73f6403546d21b35462910aa83742a9e1ad7
iammurtaza <murtazaygenius@gmail.com>
2018-07-03 16:38:16 +0530

    rev pol update

--- revPolish.cpp
+++ revPolish.cpp
@@ -6,51 +6,56 @@ int main(){
 	string input,output;
 	int cases;
 	string operators = "+-*/^";
+	freopen("input.txt", "r", stdin);
 	cin>>cases;
 	op.push('N');
 	while(cases--){
 		cin >> input;
+		output="";
 		for(int i=0;i<input.length();i++){
 			char c =input[i];
 			if(isalpha(c)){
+				//cout<<"alpha ";
 				output=output+c;
 			}
 			else if (c=='('){
+				//cout<<"open ";
 				op.push('(');
 			}
 			else if (c==')'){
-				while(op.top()!='('&&!op.empty()){
+				//cout<<"close ";
+				while(op.top()!='('){
 					output=output+op.top();
 					op.pop();
 				}
+				op.pop();
 			}
-			else if (operators.find(c)!=string::npos){
-				if(!op.empty()&&operators.find(op.top())!=string::npos){
-					while((operators.find(c)<operators.find(op.top()))&&!op.empty()/*&&operators.find(op.top())!=string::npos*/){
+			else if (operators.find(c)!=-1){ 
+				
+				if(!op.empty() && operators.find(op.top()) != -1){
+					//cout<<"op1 ";
+					while( operators.find(c)<operators.find(op.top()) && !op.empty() ){
 						output=output+op.top();
 						op.pop();
-
 					}
 				}
 				else {
+					//cout<<"op2 ";
 					op.push(c);		
 				}
-
-				
 			}
 			else{
+				//cout<<"spc ";
 				output=output+c;
 			}
 		}
-		while(op.top!='N'){
+		while(op.size()>2){
 			output=output+op.top();
 			op.pop();
 
 		}
-		cout<<output;
+		cout<<output<<'\n';
 	}
-
-
 	return 0;
 }
 	
