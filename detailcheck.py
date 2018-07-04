b7f8a2aa1060073187a84b76fd311884cda711ec
iammurtaza <murtazaygenius@gmail.com>
2018-07-04 15:05:47 +0530

    dectryption added

--- detailcheck.py
+++ detailcheck.py
@@ -6,40 +6,59 @@ import sys
 import time
 import string
 import re
+from PyPDF2 import PdfFileWriter, PdfFileReader
 from urllib.request import urlopen
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 
 def main():
-	file_url=sys.argv[1]
-	bank_type=sys.argv[2]
+	# file_url=sys.argv[1]
+	# bank_type=sys.argv[2]
 
-	if(bank_type == "icici"):
-		name = file_url.split('/')
-		name2 = name[-1].split('.')
-		st = name2[0]
-		ts = int(time.time())
-		ts = str(ts)
-		st=st+'_'+ts+'.pdf';
-		response = urlopen(file_url)
-		file = open(st, 'wb')
-		file.write(response.read())
-		file.close()
-		print(checkicici(st))
-		os.remove(st)
+	# if(bank_type == "icici"):
+	# 	name = file_url.split('/')
+	# 	name2 = name[-1].split('.')
+	# 	st = name2[0]
+	# 	ts = int(time.time())
+	# 	ts = str(ts)
+	# 	st=st+'_'+ts+'.pdf';
+	# 	response = urlopen(file_url)
+	# 	file = open(st, 'wb')
+	# 	file.write(response.read())
+	# 	file.close()
+	# 	print(checkicici(st))
+	# 	os.remove(st)
 
+	# else:
+	# 	print("NOT ICICI")
+	st = "Axis-Bank-protected.pdf"
+	print(checkicici(st))
+
+def checkicici(filename):
+	password = "gullakh@123"
+	pdfFileObj = open(filename,'rb')
+	pdfFile = PdfFileReader(pdfFileObj)
+
+	if pdfFile.isEncrypted:
+		try:
+			pdfFile.decrypt(password)
+			print('File Decrypted (PyPDF2)')
+		except:
+			command="cp "+ filename +" temp.pdf; qpdf --password='" + password + "' --decrypt temp.pdf "+ filename
+			os.system(command)
+			print('File Decrypted (qpdf)')
+			#re-open the decrypted file
+			pdfFileObj = open(filename,'rb')
+			pdfFile = PdfFileReader(pdfFileObj)
 	else:
-		print("NOT ICICI")
+		print('File Not Encrypted')
 
-def checkicici(st):
-	pdfFileObj = open(st,'rb')
-	pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-	num_pages = pdfReader.numPages
+	num_pages = pdfFile.numPages
 	count = 0
 	text = ""
 
 	while count < num_pages:
-	    pageObj = pdfReader.getPage(count)
+	    pageObj = pdfFile.getPage(count)
 	    count +=1
 	    text += pageObj.extractText()
 
@@ -65,3 +84,4 @@ if __name__ == '__main__':
 # python3 -W ignore detailcheck.py https://gullakh.s3-us-west-2.amazonaws.com/2/1530598673_ICICI6.pdf icici
 # python3 -W ignore detailcheck.py https://gullakh.s3-us-west-2.amazonaws.com/php5Dt9Xf.pdf axis
 # python3 -W ignore detailcheck.py https://gullakh.s3-us-west-2.amazonaws.com/2/1530277413_ICICI%20Stmt.pdf icici
+# python3 -W ignore detailcheck.py https://gullakh.s3-us-west-2.amazonaws.com/Statement_JAN2018_343497535.pdf icici
--- revPolish.cpp
+++ revPolish.cpp
@@ -1,61 +1,17 @@
 #include <bits/stdc++.h>
-#include <stack>
-using namespace std;	
-int main(){
-	stack<char> op;
-	string input,output;
-	int cases;
-	string operators = "+-*/^";
-	freopen("input.txt", "r", stdin);
-	cin>>cases;
-	op.push('N');
-	while(cases--){
-		cin >> input;
-		output="";
-		for(int i=0;i<input.length();i++){
-			char c =input[i];
-			if(isalpha(c)){
-				//cout<<"alpha ";
-				output=output+c;
-			}
-			else if (c=='('){
-				//cout<<"open ";
-				op.push('(');
-			}
-			else if (c==')'){
-				//cout<<"close ";
-				while(op.top()!='('){
-					output=output+op.top();
-					op.pop();
-				}
-				op.pop();
-			}
-			else if (operators.find(c)!=-1){ 
-				
-				if(!op.empty() && operators.find(op.top()) != -1){
-					//cout<<"op1 ";
-					while( operators.find(c)<operators.find(op.top()) && !op.empty() ){
-						output=output+op.top();
-						op.pop();
-					}
-				}
-				else {
-					//cout<<"op2 ";
-					op.push(c);		
-				}
-			}
-			else{
-				//cout<<"spc ";
-				output=output+c;
-			}
-		}
-		while(op.size()>2){
-			output=output+op.top();
-			op.pop();
+using namespace std;
 
-		}
-		cout<<output<<'\n';
+int main() {
+	int n1,n2,n3;
+	freopen("input.txt","r",stdin);
+	cin>>n1>>n2>>n3;
+	while(n1||n2||n3){
+		if(2*n2==n1+n3)
+			cout<<"AP "<<n3+(n3-n2)<<"\n";
+		else
+			cout<<"GP "<<n3*(n3/n2)<<"\n";
+
+		cin>>n1>>n2>>n3;
 	}
 	return 0;
 }
-	
